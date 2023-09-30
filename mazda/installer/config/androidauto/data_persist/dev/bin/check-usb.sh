b5ee88c2099ba209461b390e90b559d9154252ec
Ian Newton <i.newton@c-org.com>
2023-09-30 16:24:19 +0100

    Installer updates with modified check-usb.sh & headunit-wrapper

--- mazda/installer/config/androidauto/data_persist/dev/bin/check-usb.sh
+++ mazda/installer/config/androidauto/data_persist/dev/bin/check-usb.sh
@@ -1,6 +1,36 @@
 #!/bin/sh
 
-#sleep 20
+LOGPATH=/data/headunit.log
+SWAPCOUNT=0
+timestamp() {
+  date +"%D %T"
+}
+
+check_swapfile() {
+  USBDRV=$(ls /tmp/mnt | grep sd)
+  for USB in $USBDRV; do
+      USBPATH=/tmp/mnt/${USB}
+      SWAPFILE="${USBPATH}"/swapfile
+      if [ -e "${SWAPFILE}" ]; then
+          if [ $(free |grep 'Swap:' |grep -v grep| awk -F "[[:space:]]+" '/ /{print $2}')  -lt 1 ]; then
+              mount -o rw,remount ${USBPATH}
+              swapon ${SWAPFILE}
+              sleep 2
+              if [ $(free |grep 'Swap:' |grep -v grep| awk -F "[[:space:]]+" '/ /{print $2}')  -lt 1 ]; then
+                mkswap ${SWAPFILE}
+                swapon ${SWAPFILE}
+              fi
+              echo "Swapfile initialized $(timestamp)'\n'" >> ${LOGPATH}
+              free >> ${LOGPATH}
+              sysctl vm.swappiness=10
+              cat /proc/sys/vm/swappiness >> ${LOGPATH}
+          fi
+          break
+      fi
+  done
+}
+
+rm -f /tmp/root/usb_connect
 
 LIST="/resources/aio/misc/usb-allow.list"
 if ! [ -e $LIST ]; then
@@ -8,6 +38,7 @@ if ! [ -e $LIST ]; then
 fi
 
 while [ true ]; do
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
  count=`lsusb | grep $line|wc -l|awk '{print $1}'`
  if [ $count -gt 0 ]; then
@@ -18,6 +49,7 @@ done < "$LIST"
 if [ $count -gt 0 ]; then
   echo "USB Connected"
   if ! [ -e /tmp/root/usb_connect ]; then
+    check_swapfile
     touch /tmp/root/usb_connect
   fi
  else
@@ -27,4 +59,10 @@ if [ $count -gt 0 ]; then
   fi
 fi
 sleep 2
+
+if [ $((++SWAPCOUNT)) -gt 30 ]; then
+  check_swapfile
+  SWAPCOUNT=0
+fi
+
 done
