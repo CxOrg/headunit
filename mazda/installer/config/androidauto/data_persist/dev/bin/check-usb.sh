ff7d28b73d38106895e187bc7adb338c5d9ed9d9
Ian Newton <i.newton@c-org.com>
2023-11-12 15:59:14 +0000

    Run /jci/scripts/resetvip only on USB unplug replug in check-usb.sh

--- mazda/installer/config/androidauto/data_persist/dev/bin/check-usb.sh
+++ mazda/installer/config/androidauto/data_persist/dev/bin/check-usb.sh
@@ -1,34 +1,9 @@
 #!/bin/sh
 
 LOGPATH=/data/headunit.log
-#SWAPCOUNT=0
 timestamp() {
   date +"%D %T"
 }
-#
-# check_swapfile() {
-#   USBDRV=$(ls /tmp/mnt | grep sd)
-#   for USB in $USBDRV; do
-#       USBPATH=/tmp/mnt/${USB}
-#       SWAPFILE="${USBPATH}"/swapfile
-#       if [ -e "${SWAPFILE}" ]; then
-#           if [ $(free |grep 'Swap:' |grep -v grep| awk -F "[[:space:]]+" '/ /{print $2}')  -lt 1 ]; then
-#               mount -o rw,remount ${USBPATH}
-#               swapon ${SWAPFILE}
-#               sleep 2
-#               if [ $(free |grep 'Swap:' |grep -v grep| awk -F "[[:space:]]+" '/ /{print $2}')  -lt 1 ]; then
-#                 mkswap ${SWAPFILE}
-#                 swapon ${SWAPFILE}
-#               fi
-#               echo "Swapfile initialized $(timestamp)'\n'" >> ${LOGPATH}
-#               free >> ${LOGPATH}
-#               sysctl vm.swappiness=10
-#               cat /proc/sys/vm/swappiness >> ${LOGPATH}
-#           fi
-#           break
-#       fi
-#   done
-# }
 
 rm -f /tmp/root/usb_connect
 
@@ -49,20 +24,15 @@ done < "$LIST"
 if [ $count -gt 0 ]; then
   echo "USB Connected"
   if ! [ -e /tmp/root/usb_connect ]; then
-#    check_swapfile
     touch /tmp/root/usb_connect
   fi
  else
   echo "USB disconnect"
   if [ -e /tmp/root/usb_connect ]; then
+   /jci/scripts/resetvip
    rm -f /tmp/root/usb_connect
   fi
 fi
 sleep 2
-
-#if [ $((++SWAPCOUNT)) -gt 30 ]; then
-#  check_swapfile
-#  SWAPCOUNT=0
-#fi
 
 done
